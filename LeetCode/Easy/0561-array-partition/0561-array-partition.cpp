de516a12ffa21d2af1951aa1376c8f0910f45da7
Rabbit <109808828+Rabittitan@users.noreply.github.com>
2026-04-13 17:12:09 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0561-array-partition/0561-array-partition.cpp
+++ LeetCode/Easy/0561-array-partition/0561-array-partition.cpp
@@ -3,9 +3,9 @@ public:
     int arrayPairSum(vector<int>& nums) {
         int sum = 0;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i < nums.size(); i+=2){
             
-            if(i % 2 == 0) sum += nums[i];
+            sum += nums[i];
         }
 
         return sum;
