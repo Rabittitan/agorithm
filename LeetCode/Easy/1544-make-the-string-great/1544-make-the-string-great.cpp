a98282c95b4d222feeecd62769526c0d6a4e915a
Rabbit <109808828+Rabittitan@users.noreply.github.com>
2026-04-22 23:36:37 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1544-make-the-string-great/1544-make-the-string-great.cpp
+++ LeetCode/Easy/1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string makeGood(string s) {
+    string makeGood(string& s) {
         string result;
         for (char c : s) {
             if (!result.empty() && abs(result.back() - c) == 32) {
