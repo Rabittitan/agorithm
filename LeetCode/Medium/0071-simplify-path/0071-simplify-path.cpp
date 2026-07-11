11095d7f8083c7800f181f6ab5559b5f4c21c9c1
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-07-11 15:11:38 +0700

    Time: 10 ms (5.16%), Space: 15.2 MB (13.59%) - LeetHub

--- LeetCode/Medium/0071-simplify-path/0071-simplify-path.cpp
+++ LeetCode/Medium/0071-simplify-path/0071-simplify-path.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> st;
-        string cur, ans;
-        stringstream ss(path);
+        stack <string>  st;
+        string token ="";
+        string result ="";
+        
+        for(int i = 0; i < path.length(); i++){  
+            //tach mang
+            if(path[i] == '/' ){
+                if(token == "..") {
+                    if(!st.empty()) st.pop();
+                }else if( token != "" && token != "."  ){
+                    st.push(token); 
+                }
 
-        while (getline(ss, cur, '/')) {
-            if (cur.empty() || cur == ".") continue;
-            if (cur == "..") {
-                if (!st.empty()) st.pop();
-            } else {
-                st.push(cur);
+                token = "";
+            }else{
+                token += path[i];
             }
         }
+        
 
-        while (!st.empty()) {
-            ans = "/" + st.top() + ans;
+        if(token == ".."){
+            if(!st.empty()) st.pop();
+
+        } else if( token != "" && token != "."){
+            st.push(token);
+
+        }
+
+        while(!st.empty()){
+            result = '/' + st.top() + result;
             st.pop();
         }
 
-        return ans.empty() ? "/" : ans;
+       return result.empty() ? "/" : result;
     }
-    
-
-
 };
