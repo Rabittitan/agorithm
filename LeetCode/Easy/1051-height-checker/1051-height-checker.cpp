a50888bd813f189345025317412f081557dd3ae7
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-07-19 03:17:04 +0700

    Time: 0 ms (100%), Space: 11.5 MB (5.27%) - LeetHub

--- LeetCode/Easy/1051-height-checker/1051-height-checker.cpp
+++ LeetCode/Easy/1051-height-checker/1051-height-checker.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int heightChecker(vector<int>& heights) {
         vector<int> heights_t = heights;
-
         sort(heights_t.begin(),heights_t.end());
 
         int count = 0;
