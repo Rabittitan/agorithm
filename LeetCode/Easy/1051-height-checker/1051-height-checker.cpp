600b8bbd5cb74c8ded410cadebead2c24e5fd90d
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-07-19 03:15:39 +0700

    Time: 0 ms (100%), Space: 11.3 MB (62.03%) - LeetHub

---
+++ LeetCode/Easy/1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> heights_t = heights;
+
+        sort(heights_t.begin(),heights_t.end());
+
+        int count = 0;
+
+        for(int i = 0; i < heights.size(); i++){
+            if(heights_t[i] != heights[i] ){
+                count++ ;
+            }
+        }
+
+        return count;
+    }
+};
