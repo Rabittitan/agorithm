a86ddce296f81962d8931f9f5b5824b7573837a3
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-07-19 18:37:09 +0700

    Time: 0 ms (100%), Space: 10 MB (51.38%) - LeetHub

--- LeetCode/Easy/1046-last-stone-weight/1046-last-stone-weight.cpp
+++ LeetCode/Easy/1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+        
         priority_queue<int> pq(stones.begin(), stones.end());
 
         while(pq.size() > 1){
-            
+
             int x = pq.top();
             pq.pop();
 
