b8e0e25aa1ccdc9c1c00544bd2e5b2cef72ee712
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-07-26 18:42:55 +0700

    Time: 7 ms (38.33%), Space: 16.7 MB (98.1%) - LeetHub

---
+++ LeetCode/Medium/0385-mini-parser/0385-mini-parser.cpp
@@ -0,0 +1,76 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+         if (s[0] != '[')
+            return NestedInteger(stoi(s));
+
+        stack<NestedInteger> st;
+
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '[') {
+                st.push(NestedInteger());
+            }
+            else if (s[i] == ']') {
+                NestedInteger cur = st.top();
+                st.pop();
+
+                if (st.empty())
+                    return cur;
+
+                st.top().add(cur);
+            }
+            else if (s[i] == ',' ) {
+                continue;
+            }
+            else {
+                int sign = 1;
+                if (s[i] == '-') {
+                    sign = -1;
+                    i++;
+                }
+                int num = 0;
+                while (i < n && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+
+                i--;
+
+                st.top().add(NestedInteger(sign * num));
+            }
+        }
+        return NestedInteger();
+    }
+    
+};
