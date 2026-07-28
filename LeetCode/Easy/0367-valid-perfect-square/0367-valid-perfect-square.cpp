2deb68bb0995fc7fd50703e10e363ed1295e9fb3
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-07-28 23:00:45 +0700

    Time: 1225 ms (5.13%), Space: 7.9 MB (18.65%) - LeetHub

--- LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int base = 1;
-        while(num > 0){
-            num -= base;
-            base += 2;
-        }
+        // int base = 1;
+        // while(num > 0){
+        //     num -= base;
+        //     base += 2;
+        // }
 
-        return num == 0;
+        // return num == 0;
+
+
+        if(num==1) return true;
+        for(long long i=1;i<=num/2;i++){
+            long long square = i*i;
+            if(square==num) return true;
+        }
+        return false;
     }
 };
