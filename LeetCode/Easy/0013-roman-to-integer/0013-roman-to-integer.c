8c01c6058e226cdd8862508923fd88b7a8535a1b
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-08-04 23:00:39 +0700

    Time: 0 ms (100%), Space: 12.4 MB (6.49%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.c
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,50 +1,21 @@
-// int romanToInt(char* s) {
-//     int value[256] = {0};
-//     value['I'] = 1;
-//     value['V'] = 5;
-//     value['X'] = 10;
-//     value['L'] = 50;
-//     value['C'] = 100;
-//     value['D'] = 500;
-//     value['M'] = 1000;
+int romanToInt(char* s) {
+    int value[256] = {0};
+    value['I'] = 1;
+    value['V'] = 5;
+    value['X'] = 10;
+    value['L'] = 50;
+    value['C'] = 100;
+    value['D'] = 500;
+    value['M'] = 1000;
 
-//     int ans = 0;
+    int ans = 0;
 
-//     for (int i = 0; s[i] != '\0'; i++) {
-//         if (s[i + 1] != '\0' && value[s[i]] < value[s[i + 1]])
-//             ans -= value[s[i]];
-//         else
-//             ans += value[s[i]];
-//     }
-
-//     return ans;
-// }
-
-int value(char c)
-{
-    switch(c)
-    {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
-        default: return 0;
-    }
-}
-int romanToInt(char* s)
-{
-    int sum = 0;
-    for(int i = 0; s[i] != '\0'; i++)
-    {
-        int curr = value(s[i]);
-        int next = value(s[i + 1]);
-        if(curr < next)
-            sum -= curr;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i + 1] != '\0' && value[s[i]] < value[s[i + 1]])
+            ans -= value[s[i]];
         else
-            sum += curr;
+            ans += value[s[i]];
     }
-    return sum;
+
+    return ans;
 }
