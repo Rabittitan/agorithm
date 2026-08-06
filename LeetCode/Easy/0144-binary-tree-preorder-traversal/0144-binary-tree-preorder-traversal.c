f30745ca01b1c22b9665d0334445156f282c398c
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-08-06 22:57:21 +0700

    Time: 0 ms (100%), Space: 9.4 MB (13.68%) - LeetHub

--- LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.c
+++ LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.c
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+
 int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     int* result = (int*)malloc(100 * sizeof(int));
     *returnSize = 0;
