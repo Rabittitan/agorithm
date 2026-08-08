4c49c846bbc7030fd7d3a5285ee9a8ff05638d9c
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-08-08 23:30:41 +0700

    Time: 4 ms (41.84%), Space: 8.6 MB (96.08%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,16 +1,28 @@
 int strStr(char* haystack, char* needle) {
-    int h_len = strlen(haystack);
-    int n_len = strlen(needle);
-    
-    if (n_len > h_len) {
-        return -1;
-    }
-    
-    for (int i = 0; i <= h_len - n_len; i++) {
-        if (strncmp(&haystack[i], needle, n_len) == 0) {
-            return i;
+     int k=0;
+    int c=0;
+    int h=0;
+    for (int i=0;i<strlen(haystack);i++){
+        if (haystack[i]==needle[k]){
+            c=i;
+            k++;
+            if (k==strlen(needle)){
+                h++;
+                break;
+            }
+        }
+        else if (haystack[i]!=needle[k]){
+            i=i-k;
+            c=0;
+            k=0;
         }
     }
-    
-    return -1;
+    if (h!=0){
+        return c-strlen(needle)+1;
+
+    }
+    else if(h==0){
+        return -1;
+    }
+    return 0;
 }
