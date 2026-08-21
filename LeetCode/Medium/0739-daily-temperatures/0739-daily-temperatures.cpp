4b12413d66fea7b4e14a8b9e0a7a8d6dd78a532e
Van Tien <109808828+Rabittitan@users.noreply.github.com>
2026-08-21 23:11:26 +0700

    Time: 20 ms (63.67%), Space: 107.4 MB (34.5%) - LeetHub

---
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+
+        for(int i = 0; i < temperatures.size(); i++){
+
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                int top = st.top();
+                st.pop();
+                answer[top] = i - top;
+            }
+            st.push(i);
+        }
+        return answer;
+    }
+};
